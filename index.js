c8a2e85931d1eb9990135a34c8911db27152428a
Adilson Neto <almeidneto@gmail.com>
2016-11-24 19:46:12 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var stylus = require('express-stylus');
+var nib = require('nib');
+var join = require('path').join;
+
+var app = express();
+
+var app = express();
+app.use(stylus({
+  src: join(__dirname, 'public'),
+  use: [nib()],
+  import: ['nib']
+}));
+app.use(express.static(join(__dirname, 'public')));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.listen(8080, function() {
+  console.log("Listening on 8080");
+})
