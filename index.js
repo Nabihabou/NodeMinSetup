0714643d6782ca9e299a451565f493c298cc02b2
Adilson de Almeida Neto <almeidneto@gmail.com>
2016-11-25 13:02:42 -0300

    Gulp automatizado no node

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ var express = require('express');
 var stylus = require('express-stylus');
 var nib = require('nib');
 var join = require('path').join;
+
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var join = require('path').join;
+var pug = require('gulp-pug2');
+
 var app = express();
+
+gulp.task('default', function buildHTML() {
+  return watch('public/templates/*', function() {
+    return gulp.src(join(__dirname, 'public', 'templates', '*.pug'))
+    .pipe(pug()).pipe(gulp.dest('public/templates'));
+  })
+});
 
 app.use(stylus({
   src: join(__dirname, 'public'),
   use: [nib()],
   import: ['nib']
 }));
+
 app.use(express.static(join(__dirname, 'public')));
 
 app.set('view engine', 'pug');
@@ -17,6 +31,8 @@ app.set('views', './views');
 app.get('*', function(req, res) {
   res.render('index');
 });
+
+gulp.start('default');
 
 app.listen(8080, function() {
   console.log("Listening on 8080");
